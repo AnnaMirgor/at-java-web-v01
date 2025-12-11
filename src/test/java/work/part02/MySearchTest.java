131c11bab92ba9370f178a1ef19a9aee999560be
AnnaMirgor <amirgorodskaya@bk.ru>
2025-12-11 19:27:36 +0300

    searchtests

---
+++ src/test/java/work/part02/MySearchTest.java
@@ -0,0 +1,37 @@
+package work.part02;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class MySearchDemoTest {
+    @Test
+    public void testElementSearchMethods (){
+        open("https://slqamsk.github.io/demo/search-demo/");
+
+        By myLocator = By.id("submit-button");
+        SelenideElement myElement = Selenide.element(myLocator);
+
+        $(By.id("submit-button"));
+        $(By.id("user-password"));
+
+        $(By.name("interests"));
+        $(By.name("user_email"));
+
+        $(By.className("nav-link"));
+        $(By.className("btn"));
+
+        $(By.tagName("input"));
+        $(By.tagName("h3"));
+
+        $(By.linkText("Контакты и обратная связь"));
+        $(By.linkText("Регистрация нового пользователя в системе"));
+
+        $(By.partialLinkText("длинный"));
+        $(By.partialLinkText("Контакты"));
+    }
+}
