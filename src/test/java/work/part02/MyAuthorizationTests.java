692e3dd000e41442daad0d57eb9a8659526aba0d
AnnaMirgor <amirgorodskaya@bk.ru>
2025-12-12 12:27:57 +0300

    avtorization3

--- src/test/java/work/part02/MyAuthorizationTests.java
+++ src/test/java/work/part02/MyAuthorizationTests.java
@@ -12,10 +12,6 @@ import static com.codeborne.selenide.Selenide.open;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
 class MyAuthorizationTests {
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.browser = Browsers.FIREFOX;
-    }
 
     @BeforeEach
     void setUp() {
@@ -46,6 +42,24 @@ class MyAuthorizationTests {
         $("#password").sendKeys("stand_pass11");
         $("#loginButton").click();
         $(By.className("error")).shouldBe(visible);
+
+    }
+
+    @Test
+    public void test04LoginWrongPassword() {
+        $(By.id("username")).sendKeys("standard_user");
+        $("#password").sendKeys("stand_pass11");
+        $("#loginButton").click();
+        $("#logoutButton").shouldNotBe(visible);
+
+    }
+
+    @Test
+    public void test05LoginSuccess() {
+        $(By.id("username")).sendKeys("standard_user");
+        $("#password").sendKeys("stand_pass1");
+        $("#loginButton").click();
+        $("#logoutButton").shouldBe(visible);
 
     }
 }
