3f21b5c4275ce51e197ede078a985acd06bd23f6
AnnaMirgor <amirgorodskaya@bk.ru>
2025-12-25 16:23:15 +0300

    AT5

--- src/test/java/work/part04/AT05.java
+++ src/test/java/work/part04/AT05.java
@@ -16,15 +16,20 @@ public class AT05 {
         //Открыть сайт
         open("https://www.aviasales.ru/");
         getWebDriver().manage().window().maximize();
-        Configuration.timeout = 5_000;
+        //Configuration.timeout = 8_000;
+        sleep(10_000);
+        $("#avia_form_origin-input").click();
+        //$("#avia_form_origin-input").sendKeys("");
+        $("#avia_form_origin-input").type("Москва");
         //В поле "Куда" ввести "Санкт-Петербург"
         $("#avia_form_destination-input").type("Санкт-Петербург");
-        sleep(5_000);
+        sleep(15_000);
         //Отключить галочку в поле "Открыть Островок! в новой вкладке"
         $x("//span[@class='s__u1BTPMyjvYPx48Gd']").click();
         //Выбрать дату вылета 17.01.2026
         $x("//div[text()='Когда']").click();
-        $x("//button[@aria-label='суббота, 17 января 2026 г.']").click();
+        $x("//div[text()='Когда']").setValue("17.01.2026");
+        //$x("//button[@aria-label='суббота, 17 января 2026 г.']").click();
         //Выбрать дату возвращения 31.01.2026
         $x("//div[text()='Обратно']").click();
         $x("//button[@aria-label='суббота, 31 января 2026 г.']").click();
@@ -34,15 +39,15 @@ public class AT05 {
         $x("//div[text()='Дети']/ancestor::div[@data-test-id='number-of-children']//button[@data-test-id='increase-button']").click();
         //Нажать кнопку "Найти билеты"
         $x("//button[@data-test-id='form-submit']").click();
-        Configuration.timeout = 12_000;
-
-        //Найти рейс с отметкой "Рекомендованный", вывести на консоль цену
-        System.out.println("\nЦена рекомендованного рейса: "
-                + $x("//span[text()='Рекомендуемый']/ancestor::div[@data-test-id='ticket-preview']//div[@data-test-id='price']").text());
+        Configuration.timeout = 20_000;
 
         //Найти рейс с отметкой "Самый дешёвый", вывести на консоль цену
         System.out.println("Цена самого дешёвого рейса: "
                 + $x("//span[text()='Самый дешёвый']/ancestor::div[@data-test-id='ticket-preview']//div[@data-test-id='price']").text());
+
+        //Найти рейс с отметкой "Рекомендованный", вывести на консоль цену
+        System.out.println("\nЦена рекомендованного рейса: "
+                + $x("//span[text()='Рекомендуемый']/ancestor::div[@data-test-id='ticket-preview']//div[@data-test-id='price']").text());
 
     }
 
@@ -55,7 +60,7 @@ public class AT05 {
         open("https://www.aviasales.ru/");
         getWebDriver().manage().window().maximize();
         //Дождаться появления окна "Вход через аккаунт Google" и закрыть его (это iframe)
-        Configuration.timeout = 10_000;
+        Configuration.timeout = 15_000;
         switchTo().frame($x("//div[@id='credential_picker_container']/iframe[@class='L5Fo6c-PQbLGe']"));
         $("#close").click();
         switchTo().defaultContent();
@@ -69,7 +74,7 @@ public class AT05 {
         //Выбрать дату вылета 17.01.2026
         sleep(3_000);
         $x("//div[text()='Когда']").click();
-        sleep(3_000);
+        sleep(6_000);
         $x("//button[@aria-label='суббота, 17 января 2026 г.']").click();
         //Выбрать дату возвращения 31.01.2026
         $x("//div[text()='Обратно']").click();
@@ -80,7 +85,7 @@ public class AT05 {
         $x("//div[text()='Дети']/ancestor::div[@data-test-id='number-of-children']//button[@data-test-id='increase-button']").click();
         //Нажать кнопку "Найти билеты"
         $x("//button[@data-test-id='form-submit']").click();
-        Configuration.timeout = 10_000;
+        Configuration.timeout = 15_000;
 
         //Найти рейс с отметкой "Рекомендованный", вывести на консоль цену
         System.out.println("\nЦена рекомендованного рейса: "
