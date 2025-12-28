d7491a93c4d6c75373a160cdbce94e6e13b2a892
Anna <amirgorodskaya@bk.ru>
2025-12-28 17:11:51 +0300

    28.12.2025

--- src/test/java/work/TokioCity/MenuPage.java
+++ src/test/java/work/TokioCity/MenuPage.java
@@ -12,19 +12,48 @@ public class MenuPage {
     SelenideElement
             busket = $x("//button[@class='header__basketButton']"),
             busketClose = $x("//button[@class='basket__close']"),
+            cookie = $x("//button[text()='Принять']"),
+            download = $x("//button[@class='downloadPanel__close']"),
             logo = $x("//div[@class='navbar__logo']");
 
-    public void selectCategory(String category){
-        $x("//span[contains(text(), '" + category + "')]").shouldBe(Condition.interactable, Duration.ofSeconds(30)); // Проверяем, что элемент interactable,
+    public void cookieAlert(){
+        this.cookie.shouldBe(Condition.interactable, Duration.ofSeconds(30)); // Проверяем, что элемент interactable,
         //т.е. с ним можно взаимодействовать - добавил, т.к. возникали ошибки, что элемент не interactable
-        $x("//span[contains(text(), '" + category + "')]").shouldNotBe(Condition.readonly, Duration.ofSeconds(30)); // Проверяем, что элемент доступен для записи,
+        this.cookie.shouldNotBe(Condition.readonly, Duration.ofSeconds(30)); // Проверяем, что элемент доступен для записи,
         //т.е. с ним можно взаимодействовать - добавил, т.к. возникали ошибки, что элемент readonly
-        $x("//span[contains(text(), '" + category + "')]").click();
+        this.cookie.click();
+    }
+
+    public void downloadPanel(){
+        this.download.shouldBe(Condition.interactable, Duration.ofSeconds(30)); // Проверяем, что элемент interactable,
+        //т.е. с ним можно взаимодействовать - добавил, т.к. возникали ошибки, что элемент не interactable
+        this.download.shouldNotBe(Condition.readonly, Duration.ofSeconds(30)); // Проверяем, что элемент доступен для записи,
+        //т.е. с ним можно взаимодействовать - добавил, т.к. возникали ошибки, что элемент readonly
+        this.download.click();
+    }
+
+    public void selectCategory(String category){
+        $x("//div[@class='navbar']//span[contains(text(), '" + category + "')]")
+                .shouldBe(Condition.interactable, Duration.ofSeconds(30));
+        // Проверяем, что элемент interactable,
+        //т.е. с ним можно взаимодействовать - добавил, т.к. возникали ошибки, что элемент не interactable
+        $x("//div[@class='navbar']//span[contains(text(), '" + category + "')]")
+                .shouldNotBe(Condition.readonly, Duration.ofSeconds(30));
+        // Проверяем, что элемент доступен для записи,
+        //т.е. с ним можно взаимодействовать - добавил, т.к. возникали ошибки, что элемент readonly
+        $x("//div[@class='navbar']//span[contains(text(), '" + category + "')]").click();
         System.out.println("Выбрали категорию меню: " + $x("//span[@class= 'breadcrumbs__item']").text());
     }
 
     public void addItemToBasket(String title){
-        $x("//p[contains(text(), '" + title + "')]/ancestor::div[@class='dishesItem__content']//button[text()='В корзину']").click();
+        $x("//p[contains(text(), '"
+                + title + "')]/ancestor::div[@class='dishesItem__content']//button[text()='В корзину']")
+                .shouldBe(Condition.interactable, Duration.ofSeconds(30));
+        $x("//p[contains(text(), '"
+                + title + "')]/ancestor::div[@class='dishesItem__content']//button[text()='В корзину']")
+                .shouldNotBe(Condition.readonly, Duration.ofSeconds(30));
+        $x("//p[contains(text(), '"
+                + title + "')]/ancestor::div[@class='dishesItem__content']//button[text()='В корзину']").click();
         this.busket.click();
         $x("//div[@class='name caption1']").shouldHave(text(title));
         this.busketClose.click();
--- src/test/java/work/TokioCity/TokioCityTests.java
+++ src/test/java/work/TokioCity/TokioCityTests.java
@@ -44,8 +44,10 @@ public class TokioCityTests {
         citySelectorPage.selectCity("Санкт-Петербург");
         //Выбрать пиццу Питерскую
         MenuPage menuPage = new MenuPage();
+        menuPage.downloadPanel();
+        menuPage.cookieAlert();
         menuPage.selectCategory("Пицца");
-        menuPage.addItemToBasket("Гирос Лосось");
+        menuPage.addItemToBasket("Пицца Питерская");
         menuPage.selectCategory("Супы");
         menuPage.addItemToBasket("Рамен");
         menuPage.selectCategory("Роллы");
@@ -53,4 +55,25 @@ public class TokioCityTests {
         sleep(5_000);
     }
 
+    // 3. Октрыть категорию
+    @Test
+    public void selectCategory02 () {
+        //Выбрать город Санкт-Петербург
+        CitySelectorPage citySelectorPage = new CitySelectorPage();
+        citySelectorPage.selectCity("Санкт-Петербург");
+        //Выбрать пиццу Питерскую
+        MenuPage menuPage = new MenuPage();
+        menuPage.downloadPanel();
+        menuPage.cookieAlert();
+        $x("//div[@class='navbar']//span[contains(text(), 'Пицца')]")
+                .shouldBe(Condition.interactable, Duration.ofSeconds(30));
+        // Проверяем, что элемент interactable,
+        //т.е. с ним можно взаимодействовать - добавил, т.к. возникали ошибки, что элемент не interactable
+        $x("//div[@class='navbar']//span[contains(text(), 'Пицца')]")
+                .shouldNotBe(Condition.readonly, Duration.ofSeconds(30));
+        // Проверяем, что элемент доступен для записи,
+        //т.е. с ним можно взаимодействовать - добавил, т.к. возникали ошибки, что элемент readonly
+        $x("//div[@class='navbar']//span[contains(text(), 'Пицца')]").click();
+        System.out.println("Выбрали категорию меню: " + $x("//span[@class= 'breadcrumbs__item']").text());
+    }
 }
